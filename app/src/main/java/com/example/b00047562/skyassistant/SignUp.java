f1280fab24919db0b7df27814965d48668124bc1
karim94 <kareem_chehab@hotmail.com>
2016-04-12 21:54:46 +0400

    Added array list of countries to Sign Up activity

--- app/src/main/java/com/example/b00047562/skyassistant/SignUp.java
+++ app/src/main/java/com/example/b00047562/skyassistant/SignUp.java
@@ -1,7 +1,13 @@
 package com.example.b00047562.skyassistant;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Locale;
 
 public class SignUp extends AppCompatActivity {
 
@@ -9,5 +15,19 @@ public class SignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        Locale[] locale = Locale.getAvailableLocales();
+        ArrayList<String> countries = new ArrayList<String>();
+        String country;
+        for( Locale loc : locale ){
+            country = loc.getDisplayCountry();
+            if( country.length() > 0 && !countries.contains(country) ){
+                countries.add( country );
+            }
+        }
+        Collections.sort(countries, String.CASE_INSENSITIVE_ORDER);
+
+        Spinner citizenship = (Spinner)findViewById(R.id.country);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, countries);
+        citizenship.setAdapter(adapter);
     }
 }
