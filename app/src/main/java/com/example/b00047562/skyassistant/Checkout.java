c78b60271f6e9995b41ebc6867a3c2d8666d2834
abdu355 <abdu_sah@hotmail.com>
2016-04-13 08:41:23 +0400

    v1.5

---
+++ app/src/main/java/com/example/b00047562/skyassistant/Checkout.java
@@ -0,0 +1,13 @@
+package com.example.b00047562.skyassistant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Checkout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checkout);
+    }
+}
