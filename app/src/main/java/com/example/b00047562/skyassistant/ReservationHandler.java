e3bb63751863069d454bc1692b8498e3a1423491
karim94 <kareem_chehab@hotmail.com>
2016-04-25 19:53:37 +0400

    Created 3 new classes: 1) Flight 2) Reservation 3) ReservationHandler (Partially Incomplete)

---
+++ app/src/main/java/com/example/b00047562/skyassistant/ReservationHandler.java
@@ -0,0 +1,34 @@
+package com.example.b00047562.skyassistant;
+
+/**
+ * Created by Kareem on 4/25/2016.
+ */
+public class ReservationHandler {
+    //TODO:
+    //Add any necessary fields/variables
+    Reservation Resrv;
+
+    public ReservationHandler(Reservation resrv) {
+        Resrv = resrv;
+    }
+
+    public boolean MakeReservation(){
+
+        //if success return true, else false
+        return true;
+    }
+
+    public boolean CancelReservation(){
+
+        //if success return true, else false
+        return true;
+    }
+
+    public Reservation getResrv() {
+        return Resrv;
+    }
+
+    public void setResrv(Reservation resrv) {
+        Resrv = resrv;
+    }
+}
